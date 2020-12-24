3a8affaab4ee46d8230d009f481c88c5b4770a1a
helloImHai <1503.hai.nguyen@gmail.com>
2020-12-25 01:29:17 +0800

    add until day 12

--- Day4-14.playground/Contents.swift
+++ Day4-14.playground/Contents.swift
@@ -57,11 +57,18 @@ func checkPassword(_ password: String) throws -> Bool {
     }
     return true
 }
+
 do {
     try checkPassword("password")
     print("That password is good!")
 } catch {
     print("You can't use that password.")
+}
+
+if let result = try? checkPassword("password") {
+    print("Result was \(result)")
+} else {
+    print("Oops")
 }
 
 // Pass by reference
@@ -145,6 +152,20 @@ person1.canDrink
 
 // Child class must always call super.init
 
+class Handbag {
+    var price: Int
+    init(price: Int) {
+        self.price = price
+    }
+}
+class DesignerHandbag: Handbag {
+    var brand: String
+    init(brand: String, price: Int) {
+        self.brand = brand
+        super.init(price: price)
+    }
+}
+
 // Struct object on assignment make a copy of the old struct object,
 // class on the other points to the same place in memory
 struct A {
@@ -175,3 +196,144 @@ b.name
  changed. Because of this, classes don’t need the mutating keyword with methods that
  change properties; that’s only needed with structs.
  */
+
+// DAY 11: PROTOCOLS AND EXTENSIONS
+
+// Sounds like similar to interface, there are no implementation
+protocol Identifiable {
+    var id: String { get set }
+    func identify()
+}
+
+// Protocol oriented programming, we extend the protool we create
+extension Identifiable {
+    func identify() {
+        print("My ID is \(id).")
+    }
+}
+
+struct User: Identifiable {
+    var id: String
+}
+
+let twostraws = User(id: "twostraws")
+twostraws.identify()
+
+// Can do something like this for inheritance
+func displayID(thing: Identifiable) {
+    print("My ID is \(thing.id)")
+}
+
+// Extensions: helps us extend classes
+extension Int {
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+    
+    func squared() -> Int {
+        return self * self;
+    }
+}
+var eight = 8
+eight.squared()
+eight.isEven
+
+// Protocol extensions
+// We can extend protocols so that all structs / classes implementing will have func/derived property
+extension Collection {
+    func summarize() {
+        var names = ""
+        for name in self {
+            names += "\(name) "
+        }
+        print("There are \(count) of us: \(names)")
+    }
+}
+
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+pythons.summarize()
+beatles.summarize()
+
+// DAY 12:
+
+// OPTIONAL
+// var age: Int = nil // is wrong!
+var age: Int? = nil
+age = 13
+
+// Unwrapping
+var name: String? = nil
+// name.count // is unsafe
+
+// if let syntax lets us unwraps the optional
+if let unwrapped = name {
+    print("\(unwrapped.count) letters")
+} else {
+    print("Missing name")
+}
+
+// guard let also unwraps
+func greet(_ name: String?) {
+    // Deal with unhappy path first
+    guard let unwrapped = name else {
+        print("You didn't provide a name!")
+        return
+    }
+    
+    print("Hello, \(unwrapped)!")
+}
+greet(nil)
+greet("Urmahdur")
+
+// Force unwrap
+let five = "5"
+let numFive = Int(five) // This makes numFive an Optional Int
+let actualFive = Int(five)! // actualFive will be Int not Int?
+
+// Implicitly unwrapped optionals, only use this if we know val starts at nil then assigned
+var ageBang: Int! = nil
+ageBang = 5
+ageBang == 5 // Don't get the difference actually, probs won't use this
+
+// Nil coalescing
+var nonNilNum = nil ?? 0
+
+// Optional chaining
+/*
+ Swift provides us with a shortcut when using optionals: if you want to access something like a.b.c and b is optional, you can write a question mark after it to enable optional chaining: a.b?.c.
+ */
+let names: [String?] = ["John" , "Paul", "George", "Ringo", nil]
+let beatle1 = names[0]?.uppercased()
+let beatle2 = names[4]?.uppercased()
+
+struct PersonWithId {
+    var id: String
+    
+    init?(id: String) {
+        if id.count == 9 {
+            self.id = id
+        } else {
+            return nil
+        }
+    }
+}
+
+var p1: PersonWithId? = PersonWithId(id: "123456789") // This is a failable init, it will return optional PersonWithId
+//var p1: PersonWithId = PersonWithId(id: "123456789") // This would be wrong
+
+// Type cast
+class Animal { }
+class Fish: Animal { }
+
+class Dog: Animal {
+    func makeNoise() {
+        print("Woof!")
+    }
+}
+let pets = [Fish(), Dog(), Fish(), Dog()]
+for pet in pets {
+    if let dog = pet as? Dog {
+        dog.makeNoise()
+    }
+}
